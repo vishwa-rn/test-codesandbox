80579e8f07315b34c1cd565576a7b59c5cc5b155
viswanadh-rayavarapu <44315599+viswanadh-rayavarapu@users.noreply.github.com>
2021-12-11 13:46:06 +0000

    new addition of hello world.

--- src/index.js
+++ src/index.js
@@ -7,4 +7,8 @@ document.getElementById("app").innerHTML = `
   info about Parcel 
   <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
 </div>
+
+Hello world.
+
+Hello world.
 `;
